58d5f6083dba77c988eda1aca0cd694ad99b3e79
Andrew Wang <aw1605@berkeley.edu>
2024-05-19 11:40:46 -0700

    Clean up code, add tracking of x and u history within agent

--- agents.py
+++ agents.py
@@ -32,9 +32,11 @@ class Agent:
         self.scene_config = scene_config
         self.x = x0
         self.controller = controller
+        self.x_hist = x0
+        self.u_hist = None
 
     # Implement dynamics and update state one timestep later
-    def step(self, oppo_states):
+    def step(self, oppo_states, t):
         raise NotImplementedError("Inheritance not implemented correctly")
 
     def getLastState(self):
@@ -57,16 +59,17 @@ class BicycleVehicle(Agent):
     Implement dynamics and update state one timestep later
     oppo_states: Nxk 
     """
-    def step(self, oppo_states, curvature):
-        # accel, delta_dot = self.controller.computeControl(self.x[-1], oppo_states, curvature)
-        accel, delta_dot = 10, 0
-        x_new, alpha_f, alpha_r = self.dynamics(accel, delta_dot)
-        return x_new, alpha_f, alpha_r
+    def step(self, oppo_states, curvature, t):
+        print("\n")
+        accel, delta_dot = self.controller.computeControl(self.x[-1], oppo_states, curvature, t)
+        x_new = self.dynamics(accel, delta_dot)
+        self.x = x_new
+        self.x_hist = np.vstack((self.x_hist, self.x))
+        self.u_hist = np.vstack((self.u_hist, np.array([accel, delta_dot]))) if self.u_hist is not None else np.array([accel, delta_dot])
+        return x_new
     
     # Steps forward dynamics of vehicle one discrete timestep
     def dynamics(self, accel, delta_dot):
-        print("\n")
-
         # Expands state variable and precalculates sin/cos
         s, ey, epsi, vx, vy, omega, delta = self.x
         sin_epsi, cos_epsi = np.sin(epsi), np.cos(epsi)
@@ -82,8 +85,12 @@ class BicycleVehicle(Agent):
 
         # Calculate various forces 
         Fxf, Fxr = self.longitudinalForce(accel)
-        Fyf, Fyr, alpha_f, alpha_r = self.lateralForce()
-        Fd = 0 #self.dragForce()
+        Fyf, Fyr = self.lateralForce()
+        Fd = self.dragForce()
+
+        # _, _, track_psi = self.scene_config["track"].getTrackPosition(s)
+        # print("trackpsi", track_psi, "error psi", epsi)
+        # print("Fxf", np.round(Fxf,4), "Fxr", np.round(Fxr,4), "Fyf", np.round(Fyf,4), "Fyr", np.round(Fyr,4), )
 
         # Calculate x_dot components from dynamics equations
         s_dot = (vx*cos_epsi - vy*sin_epsi) / (1 - ey * kappa)
@@ -93,12 +100,13 @@ class BicycleVehicle(Agent):
         vy_dot = 1/m * (Fyr + Fyf*cos_delta - m*vx*omega)
         omega_dot = 1/Iz * (lf*Fyf*cos_delta - lr*Fyr)
 
-        print("vx_dot", np.round(vx_dot,4), "Fyf*sin_delta", np.round(Fyf*sin_delta,4), "Fyr", np.round(Fyr,4))
+        # print("vx_dot", np.round(vx_dot,4), "Fyf", np.round(Fyf,4), "Fyr", np.round(Fyr,4), "Fxf", np.round(Fxf,4))
 
         # Propogate state variable forwards one timestep with Euler step
         x_dot = np.array([s_dot, ey_dot, epsi_dot, vx_dot, vy_dot, omega_dot, delta_dot])
-        self.x = self.x + x_dot*dt
-        return self.x, alpha_f, alpha_r
+        print("xdot", np.round(x_dot, 4))
+        x_new = self.x + x_dot*dt
+        return x_new
 
     
     # Rear wheel drive, all acceleration goes onto rear wheels
@@ -107,15 +115,13 @@ class BicycleVehicle(Agent):
         Fxf, Fxr = 0, m*accel
         return Fxf, Fxr
     
-    
     # Simple linearized lateral forces/tire model with slip angles
     def lateralForce(self):
         c = self.veh_config["c"]
         alpha_f, alpha_r = self.slipAngles()
         Fyf, Fyr = c*alpha_f, c*alpha_r
-        return Fyf, Fyr, alpha_f, alpha_r
+        return Fyf, Fyr
     
-        
     # Slip angles for tires
     def slipAngles(self):
         vx, vy, omega, delta = self.x[3:]
@@ -125,20 +131,18 @@ class BicycleVehicle(Agent):
         if vx < 1e-3:
             alpha_f, alpha_r = 0,0
         else: 
-            # if abs((vy + lf*omega) / vx) < 0.1:
-            #     alpha_f = (vx*delta - vy - lf*omega) / vx
-            # else:
-            #     # alpha_f = delta - np.arctan2((vy + lf*omega), vx)
-            alpha_f = delta - np.arctan((vy + lf*omega) / vx)
+            if abs((vy + lf*omega) / vx) < 0.1:
+                alpha_f = (vx*delta - vy - lf*omega) / vx
+            else:
+                alpha_f = delta - np.arctan((vy + lf*omega) / vx)
             
-            # if abs((vy - lr*omega) / vx) < 0.1:
-            #     alpha_r = (-vy + lr*omega) / vx
-            # else:
-            #     # alpha_r = -np.arctan2((vy - lr*omega), vx)
-            alpha_r = -np.arctan((vy - lr*omega) / vx)
+            if abs((vy - lr*omega) / vx) < 0.1:
+                alpha_r = (-vy + lr*omega) / vx
+            else:
+                alpha_r = -np.arctan((vy - lr*omega) / vx)
                 
-
-        print(alpha_f/np.pi * 180, alpha_r/np.pi*180)
+        # print("Pre-Slip Angles", vx, vy, omega, delta)
+        print("Slip Angles [deg]", alpha_f/np.pi * 180, alpha_r/np.pi*180)
         return alpha_f, alpha_r
     
 
@@ -154,35 +158,56 @@ class BicycleVehicle(Agent):
         
 if __name__ == "__main__":
     from track import OvalTrack
-    from config import get_vehicle_config, get_scene_config
+    from config import get_vehicle_config, get_scene_config, get_sinusoidal_controller_config
     import matplotlib.pyplot as plt
+    from controllers import SinusoidalController
 
     veh_config = get_vehicle_config()
     scene_config = get_scene_config()
-    x0 = [0, 0, 0, 44, 0, 0, np.pi/180]
-    agent = BicycleVehicle(veh_config, scene_config, x0, 0)
-    x_holder = []
-    alpha_f_holder = []
-    alpha_r_holder = []
-    for i in range(5000):
-        holder, alpha_f, alpha_r = agent.step(0,0)
-        holder = np.round(holder, 4)
-        print("CL coords", holder)
-        holder = scene_config["track"].CLtoGlobal(holder)
-        # print(str(i), " Global coords", np.round(holder,2))
-        x_holder.append(holder)
-        alpha_f_holder.append(alpha_f)
-        alpha_r_holder.append(alpha_r)
+    cont_config = get_sinusoidal_controller_config()
+     
+    dt = scene_config["dt"]
+    x0 = np.array([0, 0, 0, 12, 0, 0, 0])
+    cont = SinusoidalController(veh_config, scene_config, cont_config)
+    agent = BicycleVehicle(veh_config, scene_config, x0, cont)
 
+    t_hist = [0]
+    xg_hist = [x0]
+    for i in range(4000):
+        t = t_hist[-1] + dt
+        t_hist.append(t)
+        x_cl = agent.step(0, [], t)
+        print("CL coords", np.round(x_cl,4))
+        x_g = scene_config["track"].CLtoGlobal(x_cl)
+        # print(str(i), " Global coords", np.round(x_g,2))
+        xg_hist.append(x_g)
+        # if (np.abs(holder[2]) > 5e-2):
+        #     break
 
-    x_holder = np.array(x_holder)
-    plt.figure(0)
+    def plot_cl_states(t_hist, agent):
+        titles = ["s", "ey", "epsi", "vx", "vy", "omega", "delta", "accel", "delta_dot"]
+        plt.figure(figsize=(15,8))
+        for i in range(7):
+            plt.subplot(3,3,i+1)
+            plt.plot(t_hist, agent.x_hist[:,i])
+            plt.title(titles[i])
+
+        for i in range(7,9):
+            plt.subplot(3,3,i+1)
+            plt.plot(t_hist[:-1], agent.u_hist[:,i-7])
+            plt.title(titles[i])
+        # plt.show()
+
+        
+    plot_cl_states(t_hist, agent)
+
+    plt.figure()
     scene_config["track"].plotTrack()
-    plt.plot(x_holder[:, 0], x_holder[:, 1])
-    # plt.scatter(x_holder[:, 0], x_holder[:, 1])
-
-    plt.figure(1)
-    plt.plot(alpha_f_holder)
+    xg_hist = np.array(xg_hist)
+    plt.plot(xg_hist[:, 0], xg_hist[:, 1])
     plt.show()
+    # plt.scatter(xg_hist[:, 0], xg_hist[:, 1])
+
+    # plt.show()
     
 
