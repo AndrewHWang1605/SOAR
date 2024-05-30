c83b753366ce5a874570935727f4882e5371fb4d
Andrew Wang <aw1605@berkeley.edu>
2024-05-29 20:11:29 -0700

    Add casadi dynamics function, untested

--- agents.py
+++ agents.py
@@ -25,6 +25,7 @@ SOFTWARE.
 Implement various agents 
 """
 import numpy as np
+import casadi as ca
 import copy
 
 class Agent:
@@ -85,7 +86,7 @@ class BicycleVehicle(Agent):
     def step(self, oppo_states):
         accel, delta_dot = self.controller.computeControl(self.x_hist[self.current_timestep], oppo_states, self.current_timestep*self.dt)
         accel, delta_dot = self.saturate_inputs(accel, delta_dot)
-        x_new = self.dynamics(accel, delta_dot)
+        x_new = self.dynamics(self.x, accel, delta_dot)
         self.x = x_new
         self.x_global = self.scene_config["track"].CLtoGlobal(x_new)
         self.x_hist[self.current_timestep+1, :] = copy.deepcopy(self.x)
@@ -95,9 +96,9 @@ class BicycleVehicle(Agent):
         return x_new
     
     # Steps forward dynamics of vehicle one discrete timestep
-    def dynamics(self, accel, delta_dot):
+    def dynamics(self, x, accel, delta_dot):
         # Expands state variable and precalculates sin/cos
-        s, ey, epsi, vx, vy, omega, delta = self.x
+        s, ey, epsi, vx, vy, omega, delta = x
         sin_epsi, cos_epsi = np.sin(epsi), np.cos(epsi)
         sin_delta, cos_delta = np.sin(delta), np.cos(delta)
 
@@ -111,9 +112,9 @@ class BicycleVehicle(Agent):
 
         # Calculate various forces 
         Fxf, Fxr = self.longitudinalForce(accel)
-        Fyf, Fyr = self.lateralForce()
+        Fyf, Fyr = self.lateralForce(x)
         Fxf, Fxr, Fyf, Fyr = self.saturate_forces(Fxf, Fxr, Fyf, Fyr)
-        Fd = self.dragForce()
+        Fd = self.dragForce(x)
 
         # Calculate x_dot components from dynamics equations
         s_dot = (vx*cos_epsi - vy*sin_epsi) / (1 - ey * kappa)
@@ -126,8 +127,43 @@ class BicycleVehicle(Agent):
         # Propogate state variable forwards one timestep with Euler step
         x_dot = np.array([s_dot, ey_dot, epsi_dot, vx_dot, vy_dot, omega_dot, delta_dot])
         # print("xdot", np.round(x_dot, 4))
-        x_new = self.x + x_dot*dt
+        x_new = x + x_dot*dt
         x_new[6] = np.clip(x_new[6], -self.veh_config["max_steer"], self.veh_config["max_steer"])
+        return x_new
+
+    # Steps forward dynamics of vehicle one discrete timestep for CasADi symbolic vars
+    def casadi_dynamics(self, x, accel, delta_dot, kappa):
+        # Expands state variable and precalculates sin/cos
+        s, ey, epsi, vx, vy, omega, delta = [x[i] for i in range(self.x.shape[0])]
+        sin_epsi, cos_epsi = ca.sin(epsi), ca.cos(epsi)
+        sin_delta, cos_delta = ca.sin(delta), ca.cos(delta)
+
+        # Expand scene and vehicle config variables
+        dt = self.scene_config["dt"]
+        m = self.veh_config["m"]
+        Iz = self.veh_config["Iz"]
+        lf = self.veh_config["lf"]
+        lr = self.veh_config["lr"]
+
+        # Calculate various forces 
+        Fxf, Fxr = self.longitudinalForce(accel)
+        Fyf, Fyr = self.lateralForce(x)
+        # Fxf, Fxr, Fyf, Fyr = self.saturate_forces(Fxf, Fxr, Fyf, Fyr)
+        Fd = self.dragForce(x)
+
+        # Calculate x_dot components from dynamics equations
+        s_dot = (vx*cos_epsi - vy*sin_epsi) / (1 - ey * kappa)
+        ey_dot = vx*sin_epsi + vy*cos_epsi
+        epsi_dot = omega - kappa*s_dot
+        vx_dot = 1/m * (Fxr - Fd - Fyf*sin_delta + m*vy*omega)
+        vy_dot = 1/m * (Fyr + Fyf*cos_delta - m*vx*omega)
+        omega_dot = 1/Iz * (lf*Fyf*cos_delta - lr*Fyr)
+
+        # Propogate state variable forwards one timestep with Euler step
+        x_dot = ca.SX([s_dot, ey_dot, epsi_dot, vx_dot, vy_dot, omega_dot, delta_dot])
+        # print("xdot", np.round(x_dot, 4))
+        x_new = x + x_dot*dt
+        # x_new[6] = np.clip(x_new[6], -self.veh_config["max_steer"], self.veh_config["max_steer"])
         return x_new
 
     # Rear wheel drive, all acceleration goes onto rear wheels
@@ -137,15 +173,15 @@ class BicycleVehicle(Agent):
         return Fxf, Fxr
     
     # Simple linearized lateral forces/tire model with slip angles
-    def lateralForce(self):
+    def lateralForce(self, x):
         c = self.veh_config["c"]
-        alpha_f, alpha_r = self.slipAngles()
+        alpha_f, alpha_r = self.slipAngles(x)
         Fyf, Fyr = c*alpha_f, c*alpha_r
         return Fyf, Fyr
     
     # Slip angles for tires
-    def slipAngles(self):
-        vx, vy, omega, delta = self.x[3:]
+    def slipAngles(self, x):
+        vx, vy, omega, delta = x[3:]
         lf = self.veh_config["lf"]
         lr = self.veh_config["lr"]
         eps = 1e-6 # Avoid divide by 0
@@ -158,8 +194,8 @@ class BicycleVehicle(Agent):
         return alpha_f, alpha_r
     
     # Frontal drag force of vehicle
-    def dragForce(self):
-        vx = self.x[4]
+    def dragForce(self, x):
+        vx = x[4]
         rho = 1.225
         Cd = self.veh_config["Cd"]
         SA = self.veh_config["SA"]
